06ee7927eb5493b7858475a692c8ca9754f96a12
narin111 <jungnr11@naver.com>
2021-12-05 00:49:00 +0900

    movieapp-start

---
+++ movie_app/src/components/Movie.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types"
+function Movie({coverImg, title, summary, genres}) {
+    // App.js으로부터 movie 정보를 받는다
+    return (
+        <div>
+            <img src={coverImg} alt={title} />
+            <h2>{title}</h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+
+}
+
+Movie.propTypes = {
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+export default Movie;
