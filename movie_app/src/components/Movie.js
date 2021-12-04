d4d030fb6754b3928de56f49f0d27a09d02ff9b8
narin111 <jungnr11@naver.com>
2021-12-05 02:10:13 +0900

    movie hompage

--- movie_app/src/components/Movie.js
+++ movie_app/src/components/Movie.js
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types"
-function Movie({coverImg, title, summary, genres}) {
+import {Link} from "react-router-dom"
+
+function Movie({id, coverImg, title, summary, genres}) {
     // App.js으로부터 movie 정보를 받는다
     return (
         <div>
             <img src={coverImg} alt={title} />
-            <h2>{title}</h2>
+            <h2>
+                <Link to={`/movie/${id}`}>{title}</Link>
+            </h2>
             <p>{summary}</p>
             <ul>
                 {genres.map((g) => (
@@ -17,6 +21,7 @@ function Movie({coverImg, title, summary, genres}) {
 }
 
 Movie.propTypes = {
+    id:PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
